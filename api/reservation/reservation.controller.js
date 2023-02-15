b0ba55ab27203520e9251938b10503a68a11b59e
sharonbellotech@gmail.com <doronkabaso@gmail.com>
2023-02-15 14:53:31 +0200

    reservations by date

--- api/reservation/reservation.controller.js
+++ api/reservation/reservation.controller.js
@@ -11,6 +11,16 @@ export async function getReservations(req, res) {
   }
 }
 
+export async function getReservationsByDate(req, res) {
+  const result = await getCollectionDocs(db, 'reservations_by_date', req.query.date)
+  if (!result || !result.reservations) {
+    res.send({reservations: []})
+  }
+  else {
+    res.send(result)
+  }
+}
+
 export async function addReservation(req, res) {
   const _uuid = uuidv4()
   const payload = {
@@ -30,5 +40,22 @@ export async function addReservation(req, res) {
   })
 }
 
-
+export async function addReservationByDate(req, res) {
+  const _uuid = uuidv4()
+  const payload = {
+    'id': _uuid,
+    'startHour': req.body.startHour,
+    'endHour': req.body.endHour,
+    'courtNumber': req.body.courtNumber,
+    "date": req.body.date
+  }
+  addDocument(db, "reservations_by_date", req.query.date, payload, (result) => {
+    if (!result) {
+      res.end(JSON.stringify({ "result": 0 }))
+    }
+    else {
+      res.end(JSON.stringify({ "result": 1 }))
+    }
+  })
+}
 
--- api/reservation/reservation.routes.js
+++ api/reservation/reservation.routes.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import { getReservations, addReservation, } from './reservation.controller.js'
+import { getReservations, getReservationsByDate, addReservation, addReservationByDate, } from './reservation.controller.js'
 
 export const reservationRoutes = express.Router()
 
 reservationRoutes.get('/reservations', getReservations)
+reservationRoutes.get('/reservations/date', getReservationsByDate)
 reservationRoutes.post('/reservations', addReservation)
+reservationRoutes.post('/reservations/date', addReservationByDate)
 
