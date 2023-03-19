8001622fd91bcd3e063431e4fe722c8b7c057ec1
sharonbellotech@gmail.com <doronkabaso@gmail.com>
2023-03-19 07:08:48 +0200

    change cors port

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ authListener()
 const port = process.env.PORT || 4000
 
 const corsOptions = {
-    origin: ['http://127.0.0.1:4000', 'http://127.0.0.1:4000', 'http://localhost:4000', 'http://127.0.0.1:3000', 'http://localhost:3000', 'http://localhost:4000'],
+    origin: ['http://127.0.0.1:4000', 'http://127.0.0.1:8080', 'http://localhost:4000', 'http://127.0.0.1:3000', 'http://localhost:3000', 'http://localhost:8080'],
     credentials: true,
     methods: 'GET, POST,PUT,DELETE,OPTIONS'
 }
