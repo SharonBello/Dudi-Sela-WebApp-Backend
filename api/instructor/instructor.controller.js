89c844d4eb327c236195c48a0f130cc7e1868526
sharonbellotech@gmail.com <doronkabaso@gmail.com>
2023-05-07 17:16:46 +0300

    tennis instructors from backend

--- api/instructor/instructor.controller.js
+++ api/instructor/instructor.controller.js
@@ -1,9 +1,9 @@
 import { getDocuments, db } from '../../services/db.service.js'
 
 export async function getInstructors(req, res) {
-  const result = await getDocuments(db, "tau_dudisela", 'tennis_instructors', 'tIOnNnaFBMQMx0mgZXSq')
+  const result = await getDocuments(db, "tau_dudisela", 'tennis_instructors', 'tennis_instructors')
   if (!result) {
-    res.send({instructors: []})
+    res.send({tennis_instructors: []})
   } else {
     res.send(result)
   }
