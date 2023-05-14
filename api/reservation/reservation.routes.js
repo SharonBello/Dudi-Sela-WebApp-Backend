acdf4bce9dcefed6709bc687e152ec14e33980d8
sharonbellotech@gmail.com <doronkabaso@gmail.com>
2023-05-14 11:08:50 +0300

    refactor user reservations

--- api/reservation/reservation.routes.js
+++ api/reservation/reservation.routes.js
@@ -1,19 +1,19 @@
 import express from 'express'
-import { getReservations, getUserReservations, addReservationToUser, resetScheduleByWeekDay, postScheduleByWeekDay, getScheduleByWeekDay, isReservationExists, addReservation, getCredit, changeCredit, deleteReservation, addReservationByDate } from './reservation.controller.js'
+import { getReservationByDate, getUserReservations, addReservationToUser, isReservationExists, addReservation, getCredit, changeCredit, deleteReservation, addReservationByDate } from './reservation.controller.js'
 
 export const reservationRoutes = express.Router()
 
-reservationRoutes.get('/reservations', getReservations)
+// reservationRoutes.get('/reservations', getReservationByDate)
 reservationRoutes.post('/reservation/exists', isReservationExists)
-reservationRoutes.get('/schedule/weekday', getScheduleByWeekDay)
-reservationRoutes.post('/schedule/weekday', postScheduleByWeekDay)
-reservationRoutes.post('/schedule/reset', resetScheduleByWeekDay)
+// reservationRoutes.get('/schedule/weekday', getScheduleByWeekDay)
+// reservationRoutes.post('/schedule/weekday', postScheduleByWeekDay)
+// reservationRoutes.post('/schedule/reset', resetScheduleByWeekDay)
 reservationRoutes.get('/usercredit', getCredit)
 reservationRoutes.post('/usercredit', changeCredit)
 reservationRoutes.post('/reservations/date', addReservationByDate)
 reservationRoutes.delete('/reservations', deleteReservation)
 reservationRoutes.post('/addReservation', addReservation)
 reservationRoutes.post('/addReservation/user', addReservationToUser)
-reservationRoutes.post('/userreservations', getUserReservations)
+// reservationRoutes.post('/userreservations', getUserReservations)
 reservationRoutes.post('/userreservations/user', getUserReservations)
-
+reservationRoutes.post('/userreservations/date', getReservationByDate)
