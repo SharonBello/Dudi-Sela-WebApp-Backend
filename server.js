77b73b9e9c44b171ebb2b7e5bee53b0fcbd0a6e2
sharonbellotech@gmail.com <doronkabaso@gmail.com>
2023-06-25 11:12:47 +0200

    refactor auth

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ import express from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
 import { authListener } from './services/auth_state_listener.js'
+import { getAuth, signOut, setPersistence, signInWithEmailAndPassword, browserSessionPersistence, createUserWithEmailAndPassword } from "firebase/auth"
 import path from 'path'
+import { fileURLToPath } from 'url'
 import http from 'http'
 
 const app = express()
@@ -22,26 +24,11 @@ const corsOptions = {
 }
 app.use(cors(corsOptions))
 
-import { authRoutes } from './api/auth/auth.routes.js'
 import { reservationRoutes } from './api/reservation/reservation.routes.js'
 import { courtRoutes } from './api/court/court.routes.js'
 import { eventRoutes } from './api/event/event.routes.js'
 import { instructorRoutes } from './api/instructor/instructor.routes.js'
 import admin from 'firebase-admin'
-
-
-function checkAuth(req, res, next) {
-    if (req.headers.authtoken) {
-      admin.auth().verifyIdToken(req.headers.authtoken)
-        .then(() => {
-            next()
-        }).catch(() => {
-            res.status(403).send('Unauthorized')
-        });
-    } else {
-        res.status(403).send('Unauthorized')
-    }
-}
 
 import { serviceAccount } from './.key.service.js'
 
@@ -50,15 +37,70 @@ admin.initializeApp({
     databaseURL: "https://sela-flutter-auth-default-rtdb.firebaseio.com/"
 })
 
-app.use('/', checkAuth)
+app.post('/signup', async(req, res) => {
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, req.body.email, req.body.password)
+    .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        res.json(user)
+        // ...
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+    });
+})
 
-app.use('/auth', authRoutes)
+app.post('/signin', async(req, res) => {
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, req.body.email, req.body.password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        res.json(user)
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+})
+
+app.post('/signout', async(req, res) => {
+    const auth = getAuth();
+    signOut(auth).then(() => {
+        res.end(JSON.stringify({ "result": 0 }))
+        console.log("Sign-out successful.")
+    }).catch((error) => {
+        res.end(JSON.stringify({ "result": 1 }))
+    })
+});
+
+
+// function checkAuth(req, res, next) {
+//     if (req.headers.authToken) {
+//     admin.auth().verifyIdToken(req.headers.authToken)
+//         .then(() => {
+//             next()
+//         }).catch(() => {
+//             res.status(403).send('Unauthorized')
+//         });
+//     } else {
+//         res.status(403).send('Unauthorized')
+//     }
+// }
+
+// app.use('/', checkAuth)
 app.use('/reservations', reservationRoutes)
 app.use('/courts', courtRoutes)
 app.use('/events', eventRoutes)
 app.use('/instructors', instructorRoutes)
 
 app.get('/**', (req, res) => {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
